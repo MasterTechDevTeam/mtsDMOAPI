9766b47b4224ea26524f8a2e98e17ff686d39b49
MasterTechDevTeam <69396511+MasterTechDevTeam@users.noreply.github.com>
2021-04-05 22:26:18 +0530

    Add common model for the Scheduler

---
+++ mtsDMO.Context/Utility/SchedulerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mtsDMO.Context.Utility
+{
+    public class SchedulerData
+    {
+        public Guid EventID { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+    }
+}
